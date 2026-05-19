7ed96aefb8202557b4e85df72e01f1d49ed92b48
SK <skooi866@gmail.com>
2026-05-19 23:22:08 +0800

    Initial setup of the career-os project

---
+++ career-os/backend/src/main/java/com/cowhorse/career_os/CareerOsApplication.java
@@ -0,0 +1,13 @@
+package com.cowhorse.career_os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerOsApplication.class, args);
+	}
+
+}
---
+++ career-os/backend/src/test/java/com/cowhorse/career_os/CareerOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowhorse.career_os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
